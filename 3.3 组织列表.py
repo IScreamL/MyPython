986e86b9ed324d2835025876f597d8d9cec4baa5
AN_IS <2222>
2020-04-02 17:29:49 +0800

    测试提交

--- 3.3 组织列表.py
+++ 3.3 组织列表.py
@@ -2,6 +2,11 @@
 1.sort()按与字母顺序相反的顺序排列列表元素,参数reverse=True反序
   永久性地修改了列表元素的排列顺序
 2. sorted()对列表进行临时排序
+3.reverse()永久反转列表排序，注意， reverse()不是指按与字母顺序相反的顺序排列列表元素，而只是反转列表元素的排
+列顺序：
+4.len（）确认列表的长度
+
+
 """
 
 #1.sort()
@@ -19,5 +24,11 @@ print('sorted排序后的值',cars2)
 cars2.sort()
 print('sort排序后原始值',cars2)
 
-input('请输入值',)
+# 3.reverse()
+cars3 = ['bwm','audo','toyouta','subaru']
+cars3.reverse()
+print('reverse相反排列',cars3)
 
+#4 len()
+cars4 = ['bwm','audo','toyouta','subaru']
+print(len(cars4))
---
+++ 4.1for_in.py
@@ -0,0 +1,12 @@
+"""
+1.for  in 遍历列表
+2.在for循环后面，没有缩进的代码都只执行一次，而不会重复执行。
+
+
+"""
+# 4.1  for in遍历显示
+magicians = ['alice', 'david', 'carolina']
+for magician in magicians:
+    print(magician)
+    print(magician.title() + ', that was a great trick!')
+print("Thank you , everyone . That was a great magic show!")
