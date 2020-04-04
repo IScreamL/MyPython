14e10e5b460a09a4852521e1ce427d6f5de0b623
AN_IS <2222>
2020-04-04 23:22:41 +0800

    测试提交

--- 4.1for_in.py
+++ 4.1for_in.py
@@ -8,5 +8,5 @@
 magicians = ['alice', 'david', 'carolina']
 for magician in magicians:
     print(magician)
-    print(magician.title() + ', that was a great trick!')
+    print(magician.title() + ', that was a great trick!'+'.\n')
 print("Thank you , everyone . That was a great magic show!")
---
+++ 4.3for_bug.py
@@ -0,0 +1,21 @@
+'''
+1.忘记缩进
+magicians = ['alice','david','carolina']
+for magician in magicians:
+print(magician)
+
+2.忘记缩进额外的代码行
+
+3.不必要的缩进
+message = 'Hello Python word !'
+ print(message)
+
+4.循环后不必要的缩进
+
+5.遗漏了冒号
+magicians = ['alice','david','carolina']
+for magician in magicians
+    print(magician)
+
+'''
+
